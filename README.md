53e5395808b329a72ece3e12cde3042219494028
mmasias@macbook <manuel@masiasweb.com>
2024-04-22 23:26:22 +0200

    Inicia clase TresEnRaya
    - Creado archivo README con especificaciones de la clase.
    - Añadida estructura inicial de la clase TresEnRaya:
      - Añadidos atributos privados: tablero, jugadores, turno.
      - Implementado constructor para inicializar estos atributos.
    - Agregado esqueleto del método jugar().

---
+++ README.md
@@ -0,0 +1,9 @@
+# TresEnRaya OO
+
+- [x] Definir la clase: ***TresEnRaya***
+- [x] Definir la interfaz: ***jugar()***
+- [x] Definir los atributos que hacen falta para realizar la operación de la clase: jugar()
+  - [x] Tablero - 1 tablero
+  - [x] Jugador - 2 jugadores
+  - [x] Turno - 1 turno
+- [ ]  
--- v000/TresEnRaya.java
+++ v000/TresEnRaya.java
@@ -2,6 +2,18 @@ package v000;
 
 class TresEnRaya {
     
+    private Tablero tablero;
+    private Jugador[] jugadores;
+    private Turno turno;
+
+    public TresEnRaya(){
+        tablero = new Tablero();
+        jugadores = new Jugador[2];
+        jugadores[0] = new Jugador('x');
+        jugadores[1] = new Jugador('o');
+        turno = new Turno();
+    }
+
     public void jugar(){
 
     }
