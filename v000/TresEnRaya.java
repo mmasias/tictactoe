8562248ef2272efdee3d3db8b3d04ebc86ed2afd
mmasias@macbook <manuel@masiasweb.com>
2024-04-22 23:03:44 +0200

    Añadido juego Tres en Raya 🎮
    - Creado nuevo archivo TresEnRaya.java con clase básica.
    - Implementado método jugar() vacío.
    - Agregado método main() para iniciar el juego.

---
+++ v000/TresEnRaya.java
@@ -0,0 +1,13 @@
+package v000;
+
+class TresEnRaya {
+    
+    public void jugar(){
+
+    }
+
+    public static void main(String[] args){
+        TresEnRaya tresEnRaya = new TresEnRaya();
+        tresEnRaya.jugar();
+    }
+}
